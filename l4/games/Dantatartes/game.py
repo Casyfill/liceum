4e10fb60340e6f48ebdd82167c2adcba5d1b9f7b
Philipp Kats <casyfill@gmail.com>
2019-02-28 17:41:47 -0500

    черновик распределения, новый хельм

--- l4/games/Dantatartes/game.py
+++ l4/games/Dantatartes/game.py
@@ -35,8 +35,9 @@ state = ['''
  O  |
 /|\ |
 / \ |
-   ===='''][::-1]
-
+   ===='''][::-1]  
+# Philipp: up to you but I feel reversing and using help is less intuitive than 
+# say substracting health from max_health and indexing by that
 
 class Game:
 
@@ -47,15 +48,17 @@ class Game:
         def cook_vocab(raw_vocab):
             return [' '.join(word.upper()) for word in raw_vocab]
 
-        # делает из 'И Г Р Ы' 'И _ _ У'
+        # делает из 'И Г Р Ы' 'И _ _ У'  
         def encode_word(word):
-            word.upper()
-            mid = word[1:-2]
+            # Philipp: strings are immutable, .upper returns upper case, does not change the str.
+            word.upper()  # so it should be `word = word.upper()`
+            mid = word[1:-2]  # Philipp: wrong slicing, should be word[1:-1]
             for i in range(len(mid)):
                 if mid[i] is not " ":
                     mid = f"{mid[:i]}{'_'}{mid[i + 1:]}"
             mid = f'{mid}{" "}'
-
+            # Philipp:  how about this:
+            # `"".join([word[0],] + ['_' if char != ' ' else ' ' for char in word[1:-1]]+ [word[-1],])`
             return f"{word[0]}{mid}{word[-1]}"
 
         self.vocab = cook_vocab(vocabulary)
@@ -83,6 +86,7 @@ class Game:
         if self.__word == self.__encoded_word:
             print(f"{self.__word}\nПоздравляю, вы выиграли!")
             exit()
+            
         elif self.__hp < 0:
             print("Похоже, вы проиграли ):")
             exit()
