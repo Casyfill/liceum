9c6f73c1484363ab48d044dc9ea06b3d8c10865f
Philipp Kats <casyfill@gmail.com>
2019-03-22 12:16:33 -0400

    l9

---
+++ l9/utils.py
@@ -0,0 +1,21 @@
+import seaborn as sns
+import numpy as np
+import pylab as plt
+
+
+def plot_corr_matrix(df, figsize=(11, 9)):
+    corr = df.corr()
+
+    # Generate a mask for the upper triangle
+    mask = np.zeros_like(corr, dtype=np.bool)
+    mask[np.triu_indices_from(mask)] = True
+
+    # Set up the matplotlib figure
+    f, ax = plt.subplots(figsize=figsize)
+
+    # Generate a custom diverging colormap
+    cmap = sns.diverging_palette(220, 10, as_cmap=True)
+
+    # Draw the heatmap with the mask and correct aspect ratio
+    sns.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,
+                square=True, linewidths=.5, cbar_kws={"shrink": .5})
